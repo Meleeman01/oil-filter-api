3dd6783b6dfebfb83dfcc53f0d7c53e77c68ca71
Meleeman01 <meleeman01@live.com>
2021-09-12 20:10:34 -0600

    added a body parser and simple http server.

---
+++ server.js
@@ -0,0 +1,10 @@
+const http = require('http');
+const {parse,json,text,from} = require('get-body');
+
+const requestListener = function (req, res) {
+  parse(req, req.headers)
+  .then(body => console.log(body));
+}
+
+const server = http.createServer(requestListener);
+server.listen(3333);
